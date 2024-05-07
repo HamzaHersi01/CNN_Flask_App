5f16974db47cfc66d31f72c5b88db124a7b40060
HamzaHersi01 <105432295+HamzaHersi01@users.noreply.github.com>
2024-05-07 17:14:33 +0100

    Added file upload and used bootstrap

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def index():
+def hello_word():
     return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
-    image_path = "./image/" + imagefile.filename
-
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
     return render_template('index.html')
 
 
